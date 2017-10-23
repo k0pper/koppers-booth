8750bf38e29a6f4791c845344afef1143d542453
K0pper <mial1014@hs-karlsruhe.de>
2017-10-23 23:41:22 +0200

    Continued navbar work and design

---
+++ src/app/navbar/kopper.js
@@ -0,0 +1,19 @@
+'use strict';
+$(document).ready(function () {
+
+  var el = $('#brand');
+  var kopper = 'kopper';
+
+  var i = 0;
+  (function myLoop () {
+    setTimeout(function () {
+      el.append(kopper[i]);
+      i++;
+      if (i < 6) {
+        myLoop();
+      }
+    }, 200)
+  })();
+
+
+});
